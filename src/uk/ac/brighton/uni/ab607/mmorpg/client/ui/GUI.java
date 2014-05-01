82042287d43e3962ede9a586a64875cc61ceb11d
AlmasB <toll.everon@gmail.com>
2014-05-01 09:07:48 +0100

    added basis for new GUI impl

---
+++ src/uk/ac/brighton/uni/ab607/mmorpg/client/ui/GUI.java
@@ -0,0 +1,13 @@
+package uk.ac.brighton.uni.ab607.mmorpg.client.ui;
+
+import javax.swing.JFrame;
+
+public abstract class GUI extends JFrame {
+    /**
+     *
+     */
+    private static final long serialVersionUID = -2472437923536886979L;
+
+
+
+}
